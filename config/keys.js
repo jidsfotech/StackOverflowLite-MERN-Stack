d00ef5a1d76f9da937efe2866b2a6696087cb691
Majeed Shuaib <jidsfotech@gmail.com>
2021-01-16 14:37:51 +0100

    adjusted db config

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-
-module.exports  = { 
-    secretOrKey:'secrete',
-    productionDB:'mongodb+srv://admin:admin@dev-lwfjq.mongodb.net/StackOverflowliteDB?retryWrites=true&w=majority',
-    devDB:'mongodb://127.0.0.1/stackoverflowliteDB',
-}
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 
 const mongoose = require ('mongoose');
-const { productionDB } = require  ('./config/keys');
 const app = require ('./app');
 
 mongoose.set('useNewUrlParser', true);
@@ -9,13 +8,20 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 
+const productionDB = 'mongodb+srv://admin:admin@dev-lwfjq.mongodb.net/StackOverflowliteDB?retryWrites=true&w=majority'
+const devDB = 'mongodb://127.0.0.1/stackoverflowliteDB'
+
+const db_url =  process.env.NODE_ENV == "test" ? devDB : productionDB 
+
+console.log(db_url, process.env.NODE_ENV)
+
 //connect to mongoose 
-mongoose.connect(productionDB)
+mongoose.connect(db_url)
     .then(() => console.log("Connected to Stackoverflow-lite database sucessfully"))
     .catch(err => console.log(err));
 
 // process.env.port is Heroku's port
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 9001;
 
 app.listen(port, () => console.log(`Server up and running on http://localhost:${port}`));
  
