66f4701b1a6d7bc9f243e8165e9200ba26d3a183
Majeed Shuaib <jidsfotech@gmail.com>
2021-03-20 23:34:30 +0100

    rectify heroku post build issues

--- app/app.js
+++ app/app.js
@@ -17,6 +17,11 @@ require('../config/passport')(passport);
 // serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")))
 
+// serve the React app index page 
+app.use("/", function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(usersRoutes);
 app.use(questionsRoutes);
 
